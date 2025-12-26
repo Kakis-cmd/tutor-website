9f840aa0bed8e20b315eea8e14ca53f224cb108f
Kakis-cmd <chishimbadickson2000@gmail.com>
2025-12-26 12:45:45 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css';
-import Navbar from './components/Navbar/Navbar.js';
+import Navbar from './components/Navbar/Navbar.jsx';
 import Hero from './components/Hero/Hero.jsx';  
 function App() {
   return (
